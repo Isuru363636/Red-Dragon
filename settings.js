873ca8fcca29ebd503064742709fb98f419d6c58
Isuru363636 <108875617+Isuru363636@users.noreply.github.com>
2022-07-07 17:51:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,9 +22,9 @@ global.APIKeys = {
 global.alivemsg = `My name is RED pathum🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+94760390292'] //Owner number in aive msg
-global.premium = ['+94760390292'] //Owner Number info
-global.ownernomer = '+94760390292' //Owner Number <<<
+global.owner = ['+94763320030'] //Owner number in aive msg
+global.premium = ['+9473320030'] //Owner Number info
+global.ownernomer = '+94763320030' //Owner Number <<<
 
 global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
 global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
